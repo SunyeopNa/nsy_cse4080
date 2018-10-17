946bd5edae37306ac484c0bab6f62baa3e4be95a
Sunyeop Na <coinnip@gmail.com>
2018-10-17 13:13:35 +0900

    Update HO#1/Problem_B.c

--- HO#1/Problem_B.c
+++ HO#1/Problem_B.c
@@ -3,17 +3,21 @@
 #include <signal.h> 
 #include <unistd.h> 
 // This function will handle a signal.  
-void HandleSignal(int sig, siginfo_t *si, void *context); 
+void *HandleSignal(int sig, siginfo_t *si, void *context); 
+
 int main(int argc, char *argv[])
 {
 	struct sigaction sVal;
 	pid_t myPID;
 	pid_t myG_PID;
+
 	// Specify that we will use a signal handler that takes three arguments 
 	// instead of one, which is the default.    
 	sVal.sa_flags = SA_SIGINFO;
+
 	// Indicate which function is the signal handler.    
 	sVal.sa_sigaction = HandleSignal;
+
 	myPID = getpid();
 	myG_PID = getpgid(myPID);
 	printf("\nMy process id = %d.\n", myPID);
