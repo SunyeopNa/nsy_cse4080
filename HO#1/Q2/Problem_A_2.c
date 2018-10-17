acfb8152baaa747273ffa8121c9abfa9bbb3a7fe
Sunyeop Na <coinnip@gmail.com>
2018-10-17 18:03:04 +0900

    ok

--- HO#1/Q2/Problem_A_2.c
+++ HO#1/Q2/Problem_A_2.c
@@ -3,6 +3,9 @@
 
 void *thread(void *vargp)
 {
+	// thread는 child process 개념이 아니기 때문에
+	// 아래의 line이 실행되면 프로세스 자체가 종료된다.
+	// 따라서 main()의 pthread_join 이후의 print는 진행되지 않는다
 	exit(42);
 }
 
@@ -12,5 +15,6 @@ int main()
 	pthread_t tid;
 	pthread_create(&tid, NULL, thread, NULL);
 	pthread_join(tid, (void **)&i);
+	// 여기에 도달하지 못하고 exit에 의해 프로세스 자체가 종료된다.
 	printf("%d\n", i);
 }
--- HO#1/Q2/Problem_A_4.c
+++ HO#1/Q2/Problem_A_4.c
@@ -4,6 +4,10 @@
 void *thread(void *vargp)
 {
 	pthread_detach(pthread_self());
+	printf("MAKE DELAY 1");
+	printf("MAKE DELAY 2");
+	printf("MAKE DELAY 3");
+	printf("MAKE DELAY 4");
 	pthread_exit((void*)42);
 }
 
