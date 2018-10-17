14401463e9ff26cfca74b9d585cf115d0a7b8182
Sunyeop Na <coinnip@gmail.com>
2018-10-17 18:04:36 +0900

    Update HO#1/Q2/Problem_A_4.c

--- HO#1/Q2/Problem_A_4.c
+++ HO#1/Q2/Problem_A_4.c
@@ -4,10 +4,15 @@
 void *thread(void *vargp)
 {
 	pthread_detach(pthread_self());
-	printf("MAKE DELAY 1");
-	printf("MAKE DELAY 2");
-	printf("MAKE DELAY 3");
-	printf("MAKE DELAY 4");
+	printf("MAKE DELAY 1\n");
+	printf("MAKE DELAY 2\n");
+	printf("MAKE DELAY 3\n");
+	printf("MAKE DELAY 4\n");
+	int www =0
+	for (int i = 0; i < 10000; i++)
+	{
+		www++;
+	}
 	pthread_exit((void*)42);
 }
 
